bd97773e3499d72bdb676e3e87b388f07dfe9970
ccarlenzoli <claudio@toptal.com>
2022-01-13 16:58:51 +0100

    No sec

--- src/main/java/com/kyc/Team1Application.java
+++ src/main/java/com/kyc/Team1Application.java
@@ -2,10 +2,11 @@ package com.kyc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @Configuration
 @EnableScheduling
 public class Team1Application {
